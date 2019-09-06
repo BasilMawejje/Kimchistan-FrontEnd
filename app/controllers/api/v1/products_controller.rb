bba697453807a87545ebdb5d784005f8a8f30dab
BasilMawejje <basil.mawejje@gmail.com>
2019-09-06 12:48:28 +0300

    Add product show page

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,4 +4,9 @@ class Api::V1::ProductsController < ApplicationController
     products = Product.all
     render json: products
   end
+
+  def show
+    product = Product.find(params[:id])
+    render json: product
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
       mount Stripe::Engine => '/stripe'
-      resources :products, only: [:index]
+      resources :products, only: [:index, :show]
       resources :orders, only: [:create]
       resources :restaurants, only: [:index]
       resources :payments, only: [:create]
